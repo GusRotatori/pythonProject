84099abbe5a5790c8938c0ade369ad0f2db743e7
GusRotatori <120506154+GusRotatori@users.noreply.github.com>
2024-02-07 15:57:51 -0300

    Add files via upload

---
+++ Aula_16.py
@@ -0,0 +1,11 @@
+lanche = ('Hamburguer' , 'Suco' , 'Pizza' , 'Pudim' , 'Batata Frita')
+
+for cont in range(0, len(lanche)):
+    print(lanche[cont])
+
+for pos, comida in enumerate(lanche):
+    print(f'Eu vou comer {comida} na posição {pos}')
+print('Comi pra caramba!')
+
+pessoa = ('Gustavo', 39, 'M' , 95)
+print(pessoa)
